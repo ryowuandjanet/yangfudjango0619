6d3ac92c4d0a4fadb4f93ccd64903600dbd087b5
ryowuandjanet <ryowudata@gmail.com>
2020-06-20 09:17:57 +0000

    20200620下班前-建立Land的資料表

--- case/models.py
+++ case/models.py
@@ -27,3 +27,15 @@ class Case(models.Model):
 
 	def get_absolute_url(self):
 		return reverse("case_detail",args={str(self.id)})
+		
+class Land(models.Model):
+	case = models.ForeignKey(Case, on_delete=models.CASCADE, verbose_name="縣市")
+	land_number = models.CharField(u'地號',max_length=100)
+	land_url = models.CharField(u'地號謄本',max_length=100)
+	land_area = models.DecimalField(u'地坪',max_digits=10,decimal_places=2)
+	land_holding_point_personal = models.PositiveSmallIntegerField(default=0)
+	land_holding_point_all = models.PositiveSmallIntegerField(default=0)
+
+	def __str__(self):
+		return self.land_number
+
